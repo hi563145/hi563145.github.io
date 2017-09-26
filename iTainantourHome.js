ee325c25e0e7fd8f3f392b45d2f97c572a4983ab
hi563145 <hi563145@gmail.com>
2017-09-26 18:24:52 +0800

    Centered Images/More consistant speperator flash

--- iTainantourHome.js
+++ iTainantourHome.js
@@ -25,5 +25,35 @@ function TWdate() {
 	if (TimeM < 10) {document.getElementById("01").innerHTML = "0"}
 	else {document.getElementById("01").innerHTML = " "};
 	};
+//Flash
+function Sepflash(){
+	//Set Date and Time
+	var date = new Date();
+	var Seconds = date.getUTCSeconds()
+	var Start = Seconds + 1;
+	//If Update
+	var FlashUpdate = setInterval(Flash,0);
+	function Flash(){
+		var date2 = new Date();
+		var Flashval = date2.getUTCSeconds();
+		if (Flashval == Start) {
+		document.getElementById("sep2").style.animationName = "flash";
+		document.getElementById("sep1").style.animationName = "flash";
+		clearInterval(FlashUpdate);};
+	};
+	//Clear Update
+	clearInterval(Update2)
+}
 //Update
-var Update = setInterval(TWdate, 500);
+var Update = setInterval(TWdate, 0);
+var Update2 = setInterval(Sepflash, 0);
+/* Moving Link Bar*/
+function scrollpos(){
+	//Finding my body tag
+	var body = document.getElementById("body");
+	//Find scroll Position
+	var scroll = body.scrollTop;
+	//Change link bar
+	if(scroll < 125){document.getElementById("table").style.position = "relative";}
+	else{document.getElementById("table").style.position = "fixed";document.getElementById("table").style.top = 0;document.getElementById("table").style.left = 0;};
+}
